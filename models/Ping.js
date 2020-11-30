a311a35d1207a19917922233f6432c7b2455283a
Kevin Luo <kvn.luo@gmail.com>
2020-11-30 17:44:59 -0500

    set expires time for pings after 24 hours

--- models/Ping.js
+++ models/Ping.js
@@ -1,57 +1,59 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 // NOTE: test
-const pingSchema = new Schema(
-  {
-    body: String,
-    imageUrl: String,
-    location: {
-      type: {
-        type: String, 
-        enum: ['Point'],
-        default: "Point",
-        required: true
-      },
-      coordinates: {
-        type: [Number],
-        default: [0, 0],
-        required: true
-      }
-    },
-    author: {
-      type: Schema.Types.ObjectId,
-      ref: "User",
-    },
-    comments: [
-      {
-        body: String,
-        author: {
-          type: Schema.Types.ObjectId,
-          ref: "User",
-        },
-        createdAt: {
-          type: Date,
-          default: Date.now,
-        },
-        commentHash: [String],
-      },
-    ],
-    support: [
-      {
-        supported: Boolean,
-        user: {
-          type: Schema.Types.ObjectId,
-          ref: "User"
-        }
-      }
-    ],
-    // list of hashtags contained in the ping
-    hashtagsList: [String],
+const pingSchema = new Schema({
+  body: String,
+  imageUrl: String,
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    expires: "86400s",
   },
-  { timestamps: true }
-);
+  location: {
+    type: {
+      type: String,
+      enum: ["Point"],
+      default: "Point",
+      required: true,
+    },
+    coordinates: {
+      type: [Number],
+      default: [0, 0],
+      required: true,
+    },
+  },
+  author: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+  },
+  comments: [
+    {
+      body: String,
+      author: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+      createdAt: {
+        type: Date,
+        default: Date.now,
+      },
+      commentHash: [String],
+    },
+  ],
+  support: [
+    {
+      supported: Boolean,
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    },
+  ],
+  // list of hashtags contained in the ping
+  hashtagsList: [String],
+});
 
-pingSchema.indexes({location: '2dsphere'});
+pingSchema.indexes({ location: "2dsphere" });
 
 const Ping = mongoose.model("Ping", pingSchema);
 
